c4ab5ad58dc7258f00c8afae1331768eafd19947
codeMaverick7770 <Mtauqeer7770@gmail.com>
2025-07-25 00:41:07 +0530

    Redis port mapping fixed

--- server/utils/redisClient.js
+++ server/utils/redisClient.js
@@ -3,13 +3,12 @@ import { createClient } from 'redis';
 
 const redisClient = createClient({
   socket: {
-    host: 'localhost',  // or '127.0.0.1'
-    port: 6380          // since your docker mapped 6380 -> 6379
+    host: 'localhost',  
+    port: 6379       
   }
 });
 
 redisClient.on('error', (err) => console.error('❌ Redis Client Error:', err));
 
-await redisClient.connect();  // Top-level await is allowed in ESM
-
+await redisClient.connect();  
 export default redisClient;
