c2c502668d37bae137a63dca7d0a2dbcadca0409
codeMaverick7770 <Mtauqeer7770@gmail.com>
2025-07-26 16:41:15 +0530

    Fixed dotenv installation on ec2

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,8 @@
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-dotenv.config({ path: '/home/ec2-user/OJ-Project/.env.production' }); 
-
-console.log('🔍 Loaded MONGO_URI:', process.env.MONGO_URI);
+dotenv.config({ path: '/home/ec2-user/OJ-Project/.env.production' });
 
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const MONGO_URI = process.env.MONGO_URI;
 export const PORT = process.env.PORT || 8000;
+
+console.log('🔍 MONGO_URI from config:', MONGO_URI);
