9821768d77fff5f9aff33e8a277d1f78222aecbb
codeMaverick7770 <Mtauqeer7770@gmail.com>
2025-07-26 20:14:38 +0530

    Updated config

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
-dotenv.config(); // will automatically load from .env in root directory
-
+dotenv.config();
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const MONGO_URI = process.env.MONGO_URI;
 export const PORT = process.env.PORT || 8000;
 
-console.log('🔍 Loaded MONGO_URI:', MONGO_URI); // confirm loading
+console.log('🔍 Loaded MONGO_URI:', MONGO_URI);
